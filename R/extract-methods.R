d69eac7d08a4a55722f27dcef4086b2660a83f9e
chris <chrismbaldock@gmail.com>
2021-02-22 06:45:53 +1000

    generics for otu columns

--- R/extract-methods.R
+++ R/extract-methods.R
@@ -134,6 +134,22 @@ setMethod("[", c("otu_table","index","missing","missing"), function(x, i, j, ...
   newx2<- otu_table(newx, taxa_are_rows(x) )
   return(newx2)
 })
+#' @rdname extract-methods
+#' @inheritParams base::Extract
+#' @export
+setMethod("[", c("otu_table","missing","index","ANY"), function(x, i, j, ...){
+  newx <- as(x, "matrix")[i, j, drop=FALSE]
+  newx2<- otu_table(newx, taxa_are_rows(x) )
+  return(newx2)
+})
+#' @rdname extract-methods
+#' @inheritParams base::Extract
+#' @export
+setMethod("[", c("otu_table","missing","index","missing"), function(x, i, j, ...){
+  newx <- as(x, "matrix")[i, j, drop=FALSE]
+  newx2<- otu_table(newx, taxa_are_rows(x) )
+  return(newx2)
+})
 # extract parts of sample_data
 #
 #' @export
