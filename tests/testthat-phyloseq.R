46e0ddbdf88550fa98cbbf5c59a789b671ada11d
metagenAu <74326137+metagenAu@users.noreply.github.com>
2023-02-08 18:08:52 +1000

    Update testthat-phyloseq.R

--- tests/testthat-phyloseq.R
+++ tests/testthat-phyloseq.R
@@ -1,5 +1,5 @@
 library("testthat")
-packageVersion("phyloseq")
+packageVersion("phyloseqSparse")
 # As suggested for opt-out option on testing by users, recommended by CRAN
 # http://adv-r.had.co.nz/Testing.html
 # Previously, best practice was to put all test files in inst/tests and ensure that R CMD check ran them by putting the following code in tests/test-all.R:  
@@ -10,4 +10,4 @@ packageVersion("phyloseq")
 # library(testthat)
 # test_check("yourpackage")
 # The advantage of this new structure is that the user has control over whether or not tests are installed using the –install-tests parameter to R CMD install, or INSTALL_opts = c(“–install-tests”) argument to install.packages(). I’m not sure why you wouldn’t want to install the tests, but now you have the flexibility as requested by CRAN maintainers.
-test_check("phyloseq")
+test_check("phyloseqSparse")
